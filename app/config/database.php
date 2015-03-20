d5d11d3fbb023d38efbaeebb4d58e10531fa2d15
matt <mattwilliams1993@googlemail.com>
2015-03-20 22:36:45 +0000

    hello

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'testdb',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'abjmmscbvj',
+			'username'  => 'abjmmscbvj',
+			'password'  => '9VAMjNzd6V',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
